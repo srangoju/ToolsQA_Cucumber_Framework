1eda1a3192643243f27f864eae0a47b22abac9ce
X013810 <X013810@USE2DEVP1270.ead.thomsonreuters.com>
2022-02-02 18:27:43 +0530

    TL Changes Feb 2

--- ToolsQA.CucumberFramework/src/test/java/cucumber/TestContext.java
+++ ToolsQA.CucumberFramework/src/test/java/cucumber/TestContext.java
@@ -6,10 +6,12 @@ import managers.PageObjectManager;
 public class TestContext {
 	private WebdriverManager webDriverManager;
 	private PageObjectManager pageObjectManager;
+	public ScenarioContext scenarioContext;
 	
 	public TestContext() {
 		webDriverManager = new WebdriverManager();
 		pageObjectManager = new PageObjectManager(webDriverManager.getDriver());
+		scenarioContext = new ScenarioContext();
 	}
 	
 	public WebdriverManager getWebDriverManager() {
@@ -18,6 +20,10 @@ public class TestContext {
 	
 	public PageObjectManager getPageObjectManager() {
 		return pageObjectManager;
+	}
+	
+	public ScenarioContext getScenarioContext() {
+		return scenarioContext;
 	}
 
 }
